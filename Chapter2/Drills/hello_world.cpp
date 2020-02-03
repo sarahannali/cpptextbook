79fdf6893d862a8f6205f5062575d0e362242e89
Sarah Ali <asarahali00@gmail.com>
2020-02-03 17:24:54 -0600

    first commit, added first chapter of work

---
+++ Chapter2/Drills/hello_world.cpp
@@ -0,0 +1,7 @@
+#include "C:\Users\asara\OneDrive\Desktop\GitHub\cpptextbook\Chapter2\std_lib_facilities.h.txt"
+
+int main()
+{
+ cout<<"Hello world!\n";
+ return 0;
+}
---
+++ Chapter2/Exercises/Exercise2.cpp
@@ -0,0 +1,7 @@
+#include "C:\Users\asara\OneDrive\Desktop\GitHub\cpptextbook\Chapter2\std_lib_facilities.h.txt"
+
+int main()
+{
+ cout<<"Get out of your chair\nWalk to the stairs\nWalk up the stairs\nOnce at the top, turn left\nUse the doorknob to open the first door on your left\nYou are now in the bathroom!";
+ return 0;
+}
