914628631b66eeaf494169590806064f9b8ce078
Sarah <asarahali00@gmail.com>
2020-10-11 15:18:35 -0500

    add chapter 11 work

---
+++ Chapter11/Drills/Test_output.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+main()
+{
+    int birth_year = 1999;
+    cout << "decimal:\t" << birth_year << endl << hex
+    << "hexadecimal:\t" << birth_year << endl << oct
+    << "octadecimal:\t" << birth_year << endl << dec
+    << "my age:\t" << 21 << endl;
+}
